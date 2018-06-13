0f991d96500483b18bfe80270fd31250917c6fe9
I348909 <jesse.zuo@sap.com>
2018-06-13 17:40:52 +0800

    [add] code-push-server

---
+++ code-push/mycodepushdemo/android/app/src/main/java/com/mycodepushdemo/MainApplication.java
@@ -0,0 +1,58 @@
+package com.mycodepushdemo;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
+import com.microsoft.codepush.react.CodePushBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new CodePush("l373MdjxhXA0KNikVY6akGUvZwMF4ksvOXqog",
+          MainApplication.this,
+          BuildConfig.DEBUG,
+          "http://10.130.229.22:3000"
+          )
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
